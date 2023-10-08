919bcc9b34c25d31892c6780c2799dd6148bdb09
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-08 15:03:33 -0300

    first commit

---
+++ src/main/java/com/joaolucas/hospitalJJ/HospitalJjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.hospitalJJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalJjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalJjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/hospitalJJ/HospitalJjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.hospitalJJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalJjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
