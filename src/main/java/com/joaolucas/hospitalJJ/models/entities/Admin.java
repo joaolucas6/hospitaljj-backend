ca1b9f89abdfcd8cfbd40e3c7da98522bfed6afa
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-08 16:03:46 -0300

    feat: criando entidade Admin

--- src/main/java/com/joaolucas/hospitalJJ/models/entities/Admin.java
+++ src/main/java/com/joaolucas/hospitalJJ/models/entities/Admin.java
@@ -1,4 +1,20 @@
 package com.joaolucas.hospitalJJ.models.entities;
 
-public class Admin {
+import com.joaolucas.hospitalJJ.models.enums.Genero;
+import com.joaolucas.hospitalJJ.models.enums.Role;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
+@DiscriminatorValue("admin")
+public class Admin extends User{
+    public Admin() {
+    }
+
+    public Admin(Long id, String nome, String sobrenome, LocalDate dataNascimento, String cpf, Genero genero, String email, String senha, String numeroTelefone, Role role) {
+        super(id, nome, sobrenome, dataNascimento, cpf, genero, email, senha, numeroTelefone, role);
+    }
+
 }
