0c8b9813fd355b83e5685590a6979c696ea87363
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-08 15:49:56 -0300

    feat: criando entidade Medico

--- src/main/java/com/joaolucas/hospitalJJ/models/entities/Medico.java
+++ src/main/java/com/joaolucas/hospitalJJ/models/entities/Medico.java
@@ -1,4 +1,98 @@
 package com.joaolucas.hospitalJJ.models.entities;
 
-public class Medico {
+import com.joaolucas.hospitalJJ.models.enums.Genero;
+import com.joaolucas.hospitalJJ.models.enums.Role;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@DiscriminatorValue("medico")
+public class Medico extends User {
+
+    @OneToMany(mappedBy = "medico")
+    private List<Consulta> consultas = new ArrayList<>();
+
+    @ManyToMany
+    private List<Especialidade> especialidades = new ArrayList<>();
+
+    @Column(name = "numero_registro")
+    private String numeroRegistro;
+
+    @OneToMany(mappedBy = "medico")
+    private List<SolicitacaoConsulta> solicitacoesConsulta = new ArrayList<>();
+
+    public Medico() {
+    }
+
+    public Medico(String numeroRegistro) {
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public Medico(Long id, String nome, String sobrenome, LocalDate dataNascimento, String cpf, Genero genero, String email, String senha, String numeroTelefone, Role role, String numeroRegistro) {
+        super(id, nome, sobrenome, dataNascimento, cpf, genero, email, senha, numeroTelefone, role);
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public Medico(Long id, String nome, String sobrenome, LocalDate dataNascimento, String cpf, Genero genero, String email, String senha, String numeroTelefone, Role role) {
+        super(id, nome, sobrenome, dataNascimento, cpf, genero, email, senha, numeroTelefone, role);
+    }
+
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
+    public List<Especialidade> getEspecialidades() {
+        return especialidades;
+    }
+
+    public void setEspecialidades(List<Especialidade> especialidades) {
+        this.especialidades = especialidades;
+    }
+
+    public String getNumeroRegistro() {
+        return numeroRegistro;
+    }
+
+    public void setNumeroRegistro(String numeroRegistro) {
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public List<SolicitacaoConsulta> getSolicitacoesConsulta() {
+        return solicitacoesConsulta;
+    }
+
+    public void setSolicitacoesConsulta(List<SolicitacaoConsulta> solicitacoesConsulta) {
+        this.solicitacoesConsulta = solicitacoesConsulta;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Medico medico = (Medico) o;
+        return Objects.equals(consultas, medico.consultas) && Objects.equals(especialidades, medico.especialidades) && Objects.equals(numeroRegistro, medico.numeroRegistro) && Objects.equals(solicitacoesConsulta, medico.solicitacoesConsulta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(consultas, especialidades, numeroRegistro, solicitacoesConsulta);
+    }
+
+    @Override
+    public String toString() {
+        return "Medico{" +
+                "consultas=" + consultas +
+                ", especialidades=" + especialidades +
+                ", numeroRegistro='" + numeroRegistro + '\'' +
+                ", solicitacoesConsulta=" + solicitacoesConsulta +
+                '}';
+    }
 }
