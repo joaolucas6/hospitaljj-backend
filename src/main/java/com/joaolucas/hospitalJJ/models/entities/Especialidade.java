e252f584a97c6e23d04f68770efefc3f3a34534e
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-08 15:53:49 -0300

    feat: criando entidade Especialidade

--- src/main/java/com/joaolucas/hospitalJJ/models/entities/Especialidade.java
+++ src/main/java/com/joaolucas/hospitalJJ/models/entities/Especialidade.java
@@ -1,4 +1,105 @@
 package com.joaolucas.hospitalJJ.models.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_especialidades")
 public class Especialidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_especialidades_medicos",
+            joinColumns = @JoinColumn(name = "especialidade_id"),
+            inverseJoinColumns = @JoinColumn(name = "medico_id")
+    )
+    private List<Medico> medicos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "especialidade")
+    private List<Consulta> consultas = new ArrayList<>();
+
+    public Especialidade(){}
+
+    public Especialidade(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Medico> getMedicos() {
+        return medicos;
+    }
+
+    public void setMedicos(List<Medico> medicos) {
+        this.medicos = medicos;
+    }
+
+    public List<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Especialidade that = (Especialidade) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(descricao, that.descricao) && Objects.equals(medicos, that.medicos) && Objects.equals(consultas, that.consultas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, descricao, medicos, consultas);
+    }
+
+    @Override
+    public String toString() {
+        return "Especialidade{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", medicos=" + medicos +
+                ", consultas=" + consultas +
+                '}';
+    }
 }
