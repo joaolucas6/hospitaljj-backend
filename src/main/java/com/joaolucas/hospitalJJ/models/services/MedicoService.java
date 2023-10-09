9bf0331d7fec4341a6bc378302ffdc0115d1b18b
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-09 16:13:08 -0300

    test: criando testes para MedicoService

--- src/main/java/com/joaolucas/hospitalJJ/models/services/MedicoService.java
+++ src/main/java/com/joaolucas/hospitalJJ/models/services/MedicoService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MedicoService {
-    
+
     private final MedicoRepository medicoRepository;
 
     public List<MedicoDTO> encontrarTodos(){
---
+++ src/test/java/com/joaolucas/hospitalJJ/models/services/MedicoServiceTest.java
@@ -0,0 +1,67 @@
+package com.joaolucas.hospitalJJ.models.services;
+
+import com.joaolucas.hospitalJJ.models.dto.MedicoDTO;
+import com.joaolucas.hospitalJJ.models.entities.Medico;
+import com.joaolucas.hospitalJJ.models.repositories.MedicoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MedicoServiceTest {
+
+    @Mock
+    private MedicoRepository medicoRepository;
+    private MedicoService emTeste;
+    private Medico medico;
+
+    @BeforeEach
+    void setUp() {
+        emTeste = new MedicoService(medicoRepository);
+        medico = new Medico();
+    }
+
+    @Test
+    void deveEncontrarTodosMedicos() {
+        when(medicoRepository.findAll()).thenReturn(List.of(medico));
+        var resultado = emTeste.encontrarTodos();
+        assertThat(resultado).isEqualTo(List.of(new MedicoDTO(medico)));
+    }
+
+    @Test
+    void deveEncontrarMedicoPorId() {
+        when(medicoRepository.findById(1L)).thenReturn(Optional.of(medico));
+        var resultado = emTeste.encontrarPorId(1L);
+        assertThat(resultado).isEqualTo(new MedicoDTO(medico));
+    }
+
+    @Test
+    void deveAtualizarMedico() {
+        when(medicoRepository.findById(1L)).thenReturn(Optional.of(medico));
+        when(medicoRepository.save(medico)).thenReturn(medico);
+
+        MedicoDTO paraAtualizar = new MedicoDTO(medico);
+        paraAtualizar.setNome("Davichi");
+
+        var resultado = emTeste.atualizar(1L, paraAtualizar);
+
+        assertThat(resultado).isNotNull();
+        assertThat(medico.getNome()).isEqualTo("Davichi");
+    }
+
+    @Test
+    void deveDeletarMedico() {
+        when(medicoRepository.findById(1L)).thenReturn(Optional.of(medico));
+        emTeste.deletar(1L);
+        verify(medicoRepository, times(1)).delete(medico);
+    }
+
+}
