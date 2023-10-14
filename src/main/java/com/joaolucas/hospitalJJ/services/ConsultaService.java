5747995020d9a6d2cac3e8d831b1b6a9ec677c55
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-14 10:08:47 -0300

    feat: tratando exceptions na ConsultaService

--- src/main/java/com/joaolucas/hospitalJJ/services/ConsultaService.java
+++ src/main/java/com/joaolucas/hospitalJJ/services/ConsultaService.java
@@ -1,10 +1,14 @@
 package com.joaolucas.hospitalJJ.services;
 
+import com.joaolucas.hospitalJJ.exceptions.BadRequestException;
+import com.joaolucas.hospitalJJ.exceptions.BusinessLogicException;
+import com.joaolucas.hospitalJJ.exceptions.ResourceNotFoundException;
 import com.joaolucas.hospitalJJ.models.dto.ConsultaDTO;
 import com.joaolucas.hospitalJJ.models.entities.Consulta;
 import com.joaolucas.hospitalJJ.models.entities.Medico;
 import com.joaolucas.hospitalJJ.models.enums.Status;
 import com.joaolucas.hospitalJJ.repositories.ConsultaRepository;
+import com.joaolucas.hospitalJJ.utils.ValidacaoDeDados;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +25,13 @@ public class ConsultaService {
     }
 
     public ConsultaDTO encontrarPorId(Long id){
-        return new ConsultaDTO(consultaRepository.findById(id).orElseThrow());
+        return new ConsultaDTO(consultaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Consulta não foi encontrada com ID: " + id)));
     }
 
     public ConsultaDTO atualizar(Long id, ConsultaDTO consultaDTO){
-        Consulta consulta = consultaRepository.findById(id).orElseThrow();
+        if(!ValidacaoDeDados.validarDadosDaConsulta(consultaDTO)) throw new BadRequestException("Dados da consulta são inválidos");
+
+        Consulta consulta = consultaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Consulta não foi encontrada com ID: " + id));
 
         if(consultaDTO.getPreco() != null) consulta.setPreco(consultaDTO.getPreco());
         if(consultaDTO.getReceitaMedica() != null) consulta.setReceitaMedica(consultaDTO.getReceitaMedica());
@@ -37,8 +43,8 @@ public class ConsultaService {
             Medico medico = consulta.getMedico();
 
             // verificar se o horário de consulta está disponível
-            if(!medico.getConsultas().stream().filter(c -> consultaDTO.getHorarioInicio().isAfter(c.getHorarioInicio()) && consultaDTO.getHorarioTermino().isBefore(c.getHorarioTermino())).toList().isEmpty()) throw new RuntimeException();
-            if(!medico.getSolicitacoesConsulta().stream().filter(s -> consultaDTO.getHorarioInicio().isAfter(s.getInicioDaConsulta()) && consultaDTO.getHorarioTermino().isBefore(s.getFimDaConsulta())).toList().isEmpty()) throw new RuntimeException();
+            if(!medico.getConsultas().stream().filter(c -> consultaDTO.getHorarioInicio().isAfter(c.getHorarioInicio()) && consultaDTO.getHorarioTermino().isBefore(c.getHorarioTermino())).toList().isEmpty()) throw new BusinessLogicException("Horário desejado já está ocupado");
+            if(!medico.getSolicitacoesConsulta().stream().filter(s -> consultaDTO.getHorarioInicio().isAfter(s.getInicioDaConsulta()) && consultaDTO.getHorarioTermino().isBefore(s.getFimDaConsulta())).toList().isEmpty()) throw new BusinessLogicException("Horário desejado já está ocupado");
 
 
             consulta.setHorarioInicio(consultaDTO.getHorarioInicio());
@@ -49,17 +55,17 @@ public class ConsultaService {
     }
 
     public void deletar(Long id){
-        Consulta consulta = consultaRepository.findById(id).orElseThrow();
+        Consulta consulta = consultaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Consulta não foi encontrada com ID: " + id));
         consultaRepository.delete(consulta);
     }
 
     public void cancelarConsulta(Long consultaId){
-        Consulta consulta = consultaRepository.findById(consultaId).orElseThrow();
+        Consulta consulta = consultaRepository.findById(consultaId).orElseThrow(() -> new ResourceNotFoundException("Consulta não foi encontrada com ID: " + consultaId));
         consulta.setStatus(Status.CANCELADO);
     }
 
     public void concluirConsulta(Long consultaId){
-        Consulta consulta = consultaRepository.findById(consultaId).orElseThrow();
+        Consulta consulta = consultaRepository.findById(consultaId).orElseThrow(() -> new ResourceNotFoundException("Consulta não foi encontrada com ID: " + consultaId));
         consulta.setStatus(Status.CONCLUIDO);
     }
 }
