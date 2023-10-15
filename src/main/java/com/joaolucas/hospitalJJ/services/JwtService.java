f7b461a48d3ea647cc1cd86f0fdfe0047e8ed1b5
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-10-15 09:58:13 -0300

    feat: criando JwtService

---
+++ src/main/java/com/joaolucas/hospitalJJ/services/JwtService.java
@@ -0,0 +1,45 @@
+package com.joaolucas.hospitalJJ.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${application.security.jwt.expiration}")
+    private long expiration;
+
+    public JwtService() {
+    }
+
+    public JwtService(String SECRET_KEY, long expiration) {
+        this.SECRET_KEY = SECRET_KEY;
+        this.expiration = expiration;
+    }
+
+    public String gerarToken(UserDetails user){
+        return JWT
+                .create()
+                .withSubject(user.getUsername())
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                .sign(Algorithm.HMAC256(SECRET_KEY));
+    }
+
+    public String validarToken(String token){
+        return JWT
+                .require(Algorithm.HMAC256(SECRET_KEY))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+}
